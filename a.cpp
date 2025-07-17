a8109b2fdd4aa3da8afb352e1032c7060e80fc5e
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-07-18 00:07:59 +0530

    a

--- a.cpp
+++ a.cpp
@@ -12,7 +12,41 @@ class Solution {
                 s.insert(row);
                 s.insert(col);
                 s.insert(box);
-            }
+                class Solution {
+                    public:
+                        bool isValid(string s){
+                            stack<char> stk;
+                            for(int i=0;i<s.size();i++){
+                                if(s[i]=='(') stk.push(s[i]);
+                                else{
+                                    if(stk.empty()) return false;
+                                    else stk.pop();
+                                }
+                            }
+                            return stk.empty();
+                        }
+                    
+                        void solve(string curr,int n,vector<string> &ans){
+                                if(curr.length()==2*n){
+                                    if(isValid(curr)){
+                                        ans.push_back(curr);
+                                    }
+                                    return;
+                                }
+                                curr.push_back('(');
+                                solve(curr, n, ans);
+                                curr.pop_back();
+                                curr.push_back(')');
+                                solve(curr, n, ans);
+                                curr.pop_back();
+                            }
+                        vector<string> generateParenthesis(int n) {
+                            vector<string> ans;
+                            string curr = "";
+                            solve(curr, n, ans);
+                            return ans;
+                        }
+                    };
            } 
            return true;
         }
